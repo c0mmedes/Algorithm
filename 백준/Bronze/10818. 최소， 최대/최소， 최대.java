6bd9cf7ac03bad378a2fe4b8df3d320dece758bd
daegyeong.lee <110614907+c0mmedes@users.noreply.github.com>
2023-01-29 12:54:02 +0900

    [Bronze III] Title: 최소, 최대, Time: 1404 ms, Memory: 300296 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); // H시
+		
+		int maxNum = Integer.MIN_VALUE;
+		int minNum = Integer.MAX_VALUE;
+		
+		for(int i = 0 ; i < N; i++) {
+			int M = sc.nextInt();
+			if (M > maxNum) maxNum = M;
+			if (M < minNum) minNum = M;
+		}
+		
+		System.out.println(minNum + " " + maxNum);
+		
+	}
+
+}
+
+
