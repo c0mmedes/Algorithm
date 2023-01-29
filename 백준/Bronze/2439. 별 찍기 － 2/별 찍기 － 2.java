46e9da162b87fa456324c8ac2014ded2b2e12d31
daegyeong.lee <110614907+c0mmedes@users.noreply.github.com>
2023-01-29 13:01:18 +0900

    Update 별 찍기 － 2.java

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt(); // H시
+		int N = sc.nextInt(); 
 		
 		for (int i = 0; i < N; i++) {
 			
