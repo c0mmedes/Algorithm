22b7b61b3ccd269764b418c292d248e787c93fa2
daegyeong.lee <110614907+c0mmedes@users.noreply.github.com>
2023-01-31 23:48:41 +0900

    [Bronze V] Title: 아스키 코드, Time: 112 ms, Memory: 12756 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// String 형으로 받고 첫번째 값 
+		char c =  sc.next().charAt(0);	
+		
+		System.out.println((int) c);
+	
+	}
+
+}
+
+
