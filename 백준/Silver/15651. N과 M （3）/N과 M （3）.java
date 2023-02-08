f43c107a47d5dffbc8be0e78b37c5c802d4d1bca
daegyeong.lee <110614907+c0mmedes@users.noreply.github.com>
2023-02-08 14:32:11 +0900

    [Silver III] Title: N과 M (3), Time: 596 ms, Memory: 271304 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+	static int N, M;
+	static int[] numbers;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt(); // 1부터 N까지
+		M = sc.nextInt(); // 길이가 M인 수열
+		sb = new StringBuilder();
+		
+		numbers = new int[M];
+		
+		perm(0);
+		System.out.println(sb);
+	}
+
+	private static void perm(int cnt) {
+		
+		if (cnt == M) {
+			for (int num : numbers) {
+				sb.append(num + " ");
+			}
+			sb.append("\n");
+			
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			numbers[cnt] = i+1;
+			perm(cnt + 1);
+		}
+	}
+}
