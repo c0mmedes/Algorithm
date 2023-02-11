61c4f8aa54ecc12118fccce493a3aac9d252dcd1
daegyeong.lee <110614907+c0mmedes@users.noreply.github.com>
2023-02-11 21:38:40 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int L = sc.nextInt(); // 문자열의 길이
+		
+		String str = sc.next(); // 문자열
+		
+		double arr[] = new double[L];
+		
+		int sum = 0;
+		
+		// a~z를 1~26으로 바꿔서 배열에 넣어주기
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) str.charAt(i) - 96;
+			arr[i] = (arr[i] * Math.pow(31, i)) % 1234567891;
+			sum += arr[i];
+		}
+		
+		System.out.println(sum);
+	}
+}
