f92e2a93331e9d7fefa91ea912ad00a41b181168
daegyeong.lee <110614907+c0mmedes@users.noreply.github.com>
2023-02-13 21:16:57 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 84 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 0; tc < T; tc++) {
+			int k = Integer.parseInt(br.readLine()); // k층 
+			int n = Integer.parseInt(br.readLine()); // n호
+			
+			int arr[][] = new int[16][16];
+			
+			for (int i = 0 ; i < 16; i++) {
+				arr[i][1] = 1;
+				arr[0][i] = i;
+			}
+		
+			
+			// 구하고자 하는 층의 사람 수는 전 층의 같은 호수 + 같은 층의 전 호수 의 합
+			for (int i = 1; i < 16; i++) {
+				for (int j = 2; j < 16; j++) {
+					arr[i][j] = arr[i-1][j] + arr[i][j-1];
+				}
+			}
+			System.out.println(arr[k][n]);
+		}
+	}
+}
