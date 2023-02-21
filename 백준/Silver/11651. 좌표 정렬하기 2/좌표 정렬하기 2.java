9997065f08a30d58ecb123bbfb9531d54a5587ce
daegyeong.lee <110614907+c0mmedes@users.noreply.github.com>
2023-02-21 12:06:16 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2660 ms, Memory: 91424 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class coor{
+	int x;
+	int y;
+	
+	public coor(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		coor[] p = new coor[N];
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			p[i] = new coor(x, y); 
+		}
+		
+		Arrays.sort(p, new Comparator<coor>() {
+			@Override
+			public int compare(coor p1, coor p2) {
+				if (p1.y == p2.y) {
+					return p1.x - p2.x;
+				} 
+				return p1.y - p2.y;
+			}
+		});
+		
+		for (int i = 0; i < p.length; i++) {
+			System.out.println(p[i].x + " " + p[i].y);
+		}
+	}
+}
