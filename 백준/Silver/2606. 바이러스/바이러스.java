3e6141b3d8d7768db1522905f4df223248c8ab82
aschiamo <110614907+c0mmedes@users.noreply.github.com>
2023-03-14 19:21:50 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,53 +1,63 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int V;
-	static int [][] am;
-	static int count = 0;
+	static int arr[][];
+	static boolean visited[];
+	static int count, N= 0;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		Scanner sc = new Scanner(System.in);
-		V = sc.nextInt();
-		int E = sc.nextInt();
+		N = Integer.parseInt(br.readLine()); // 컴퓨터의 수
+		int M = Integer.parseInt(br.readLine()); // 컴퓨터의 수
+		arr = new int[N+1][N+1];
+		visited = new boolean[N+1];
 		
-		am = new int[V+1][V+1];
-		
-		int from, to;
-		
-		for (int i = 0; i < E; i++) {
-			from = sc.nextInt();
-			to = sc.nextInt();
+		for (int j = 0; j < M; j++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int n1 = Integer.parseInt(st.nextToken());
+			int n2 = Integer.parseInt(st.nextToken());
 			
-			am[from][to] = am[to][from] = 1;
+			arr[n1][n2] = arr[n2][n1] = 1;
 		}
-		
-		bfs(1);
+	
+		dfs(1);
+//		bfs(1);
 		System.out.println(count);
 	}
 
 	private static void bfs(int start) {
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		boolean[] visited = new boolean[V +1];
+		Queue<Integer> q = new ArrayDeque<>();
 		
 		q.offer(start);
 		visited[start] = true;
 		
-		int current = 0;
-		
 		while(!q.isEmpty()) {
-			current = q.poll();
+			int current = q.poll();
 			
-			for (int i = 0; i < V+1; i++) {
-				if(am[current][i] != 0 && !visited[i]) {
+			for (int i = 1; i < N + 1; i++) {
+				if (arr[current][i] == 1 && !visited[i]) {
 					q.offer(i);
 					visited[i] = true;
 					count++;
 				}
 			}
-			
+		}
+	}
+
+	private static void dfs(int start) {
+		visited[start] = true;
+		
+		for (int i = 1; i < N+1; i++) {
+			if(arr[start][i] == 1 && !visited[i]) {
+				visited[i] = true;
+				dfs(i);
+				count++;
+			}
 		}
 	}
 }
