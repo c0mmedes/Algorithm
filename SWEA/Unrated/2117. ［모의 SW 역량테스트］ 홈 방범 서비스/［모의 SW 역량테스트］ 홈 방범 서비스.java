925f15edc2e4cc12887ae8c471320a1949983d42
대경 <110614907+c0mmedes@users.noreply.github.com>
2023-05-21 17:01:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 376 ms, Memory: 87,552 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -1,23 +1,21 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Solution {
 	static int N, M;
-	static int K;
-	static int homeCnt;
-	static int[][] map;
-	static int[] dx = {1, -1, 0, 0}; 
-	static int[] dy = {0, 0, -1, 1};
-	static int res;
-	static int p;
-	static class Coor{
-		int x,y;
+	static int map[][];
+	static boolean visited[][];
+	static int res, cnt;
+	static int dr[] = {0, 0, -1, 1}; 
+	static int dc[] = {-1, 1, 0, 0};
+	static class Home {
+		int x, y;
 
-		public Coor(int x, int y) {
+		public Home(int x, int y) {
 			super();
 			this.x = x;
 			this.y = y;
@@ -26,66 +24,65 @@ public class Solution {
 	}
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("data/2117.txt"));
-		Scanner sc = new Scanner(System.in);
-		int TC = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		for(int t = 1; t <= TC; t++) {
-			res = -1; 
-			N = sc.nextInt();
-			M = sc.nextInt();
-			map = new int[N][N];
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < N; j++) {
-					map[i][j] = sc.nextInt();
-				}
-			} 
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); // 도시의 크기
+			M = Integer.parseInt(st.nextToken()); // 집이 지불할 수 있는 비용
+			res = -1;
 			
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < N; j++) {
-					homeCnt = 0;
-					p = 0;
-					K = 1;
+			map = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
 					bfs(i, j);
 				}
-			} 
-			
-			System.out.println("#" + t + " " + res);
+			}
+			System.out.println("#" + tc + " " + res);
 		}
-
 	}
+	
 	private static void bfs(int sx, int sy) {
-		boolean v[][] = new boolean[N][N];
-		Queue<Coor> q = new ArrayDeque<>();
-		q.offer(new Coor(sx, sy));
-		v[sx][sy] = true;
+		visited = new boolean[N][N];
+		visited[sx][sy] = true;
+		Queue<Home> q = new ArrayDeque<>();
+		q.offer(new Home(sx, sy));
+		cnt = 0;
 		
+		int K = 1;
 		while(!q.isEmpty()) {
 			int size = q.size();
 			while(size-- > 0) {
-				Coor current = q.poll();
-				int x = current.x;
-				int y = current.y;
-				if (map[x][y] == 1) homeCnt++;
-				for(int d = 0; d < 4; d++) {
-					int nx = x + dx[d];
-					int ny = y + dy[d];
+				Home home = q.poll();
+				if(map[home.x][home.y] == 1) cnt++;
+				
+				for (int d = 0; d < 4; d++) {
+					int nx = home.x + dr[d];
+					int ny = home.y + dc[d];
 					
-					// 범위 벗어나면 무시
-					if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-					// 방문했던곳이면 무시
-					if (v[nx][ny]) continue;
+					if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue; // 범위 넘어갈 때
+					if(visited[nx][ny]) continue; // 방문했던 곳
 					
-					q.offer(new Coor(nx, ny));
-					v[nx][ny] = true;
+					q.offer(new Home(nx, ny));
+					visited[nx][ny] = true;
 				}
+				
 			}
-			p = (M * homeCnt) - ((K * K) + (K - 1) * (K - 1));
-			if (p >= 0) {
-				res = Math.max(res, homeCnt);
+			int money = K * K + (K - 1) * (K - 1);
+			if(cnt * M - money >= 0) {
+				res = Math.max(res, cnt);
 			}
 			K++;
+			
 		}
 		
 	}
-
 }
