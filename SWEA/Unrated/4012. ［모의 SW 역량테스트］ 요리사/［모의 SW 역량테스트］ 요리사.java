a19c47fbd2e614db52c4ebf2a135e7c49c692d81
대경 <110614907+c0mmedes@users.noreply.github.com>
2023-05-31 13:46:06 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 238 ms, Memory: 23,260 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -4,66 +4,60 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int N, min;
-	static int food[][];
-	static int choice[][];
+	static int N;
+	static int map[][];
 	static boolean visited[];
+	static int res;
 	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("data/4012input.txt"));
-		StringBuilder sb = new StringBuilder();
+		//System.setIn(new FileInputStream("data/4012.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); // TEST CASE
-        
-        for (int tc = 1; tc <= T; tc++) {
-        	N = Integer.parseInt(br.readLine()); // N개의 줄에 N개의 식재료
-        	food = new int[N][N];
-        	visited = new boolean[N];
-        	min = Integer.MAX_VALUE;
-        	
-        	for (int i = 0; i < N; i++) {
-        		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        		for (int j = 0; j < N; j++) {
-        			food[i][j] = Integer.parseInt(st.nextToken());
-        		}
-        	}
-        	
-        	comb(0, 0);
-        	sb.append('#').append(tc).append(' ').append(min).append('\n');
-        }   
-        
-        System.out.println(sb);
+		for(int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			res = Integer.MAX_VALUE;
+			visited = new boolean[N];
+			map = new int[N][N];
+			for(int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			} 
+			
+			comb(0, 0);
+			
+			System.out.println("#" + tc + " " + res);
+		}
 	}
 	private static void comb(int cnt, int start) {
-		if(cnt == N/2) {
+		if(cnt==N/2) {
 			check();
 			return;
 		}
 		
-		for (int i = start; i < N; i++) {
+		for(int i= start; i < N; i++) {
 			visited[i] = true;
-			comb(cnt + 1, i + 1);
+			comb(cnt+1, i+1);
 			visited[i] = false;
 		}
-		
 	}
 	private static void check() {
-		int aSum = 0;
-		int bSum = 0;
-		int result = 0;
+		int asum=0;
+		int bsum=0;
+		int result=0;
 		
 		for (int i = 0; i < N-1; i++) {
-			for (int j = i + 1; j < N; j++) {
-				if (visited[i] && visited[j]) {
-					aSum += food[i][j] + food[j][i];
-				} else if (!visited[i] && !visited[j]){
-					bSum += food[i][j] + food[j][i];
+			for( int j = i + 1; j < N; j++) {
+				if(visited[i] && visited[j]) {
+					asum += map[i][j] + map[j][i];
+				} else if(!visited[i] && !visited[j]) {
+					bsum += map[i][j] + map[j][i];
 				}
 			}
 		}
 		
-		result = Math.abs(aSum - bSum);
-		min = Math.min(min, result);
+		result = Math.abs(asum-bsum);
+		res = Math.min(result, res);
 	}
-	
 }
