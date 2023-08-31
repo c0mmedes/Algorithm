dcf89a0a3de67e52a46d9df3d2956ef297de9449
대경 <110614907+c0mmedes@users.noreply.github.com>
2023-08-31 20:51:47 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,63 +6,70 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Coor {
-		int x, y;
+    static int N,M, arr[][];
+    static int dr[] = {-1, 1, 0, 0};
+    static int dc[] = {0, 0, -1, 1};
+    static class Coor {
+        int x, y;
 
-		public Coor(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
-	}
-	
-	static int[][] map, visited;
-	static int N, M;
-	static int[] dr = {0, 0, -1, 1};
-	static int[] dc = {1, -1, 0, 0};
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		// N * M 크기의 공간
-		N = Integer.parseInt(st.nextToken()); 
-		M = Integer.parseInt(st.nextToken());  
-		
-		map = new int[N][M];
-		visited = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < M; j++) {
-				map[i][j] = str.charAt(j) - '0';
-				visited[i][j] = str.charAt(j) - '0';
-			}
-		}
-		
-		bfs(new Coor(0, 0));
-		System.out.println(visited[N-1][M-1]);
-		
-	}
-	private static void bfs(Coor coor) {
-		Queue<Coor> q = new ArrayDeque<>();
-		
-		q.offer(coor);
-		
-		while(!q.isEmpty()) {
-			Coor current = q.poll();
-			
-			for (int d = 0; d < 4; d++) {
-				int nx = current.x + dr[d];
-				int ny = current.y + dc[d];
-				
-				if(0 <= nx && nx < N && 0 <= ny && ny < M && map[nx][ny] == 1) {
-					q.offer(new Coor(nx, ny));
-					if (visited[nx][ny] < visited[current.x][current.y] + map[nx][ny]) {
-						visited[nx][ny] = visited[current.x][current.y] + map[nx][ny];
-						map[nx][ny] = 0;
-					}
-				}
-			}
-		}
-		
-	}
+        public Coor (int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // N개의 줄에
+        M = Integer.parseInt(st.nextToken()); // M개의 정수
+
+        arr = new int[N][M];
+
+        for (int i = 0; i < N; i++){
+            String s = br.readLine();
+            for (int j = 0; j < M; j++){
+                arr[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+//        for (int i = 0; i < N; i++){
+//            System.out.println(Arrays.toString(arr[i]));
+//        }
+
+        bfs(new Coor(0, 0));
+
+        System.out.println(arr[N-1][M-1]);
+
+//        for (int i = 0; i < N; i++){
+//            System.out.println(Arrays.toString(arr[i]));
+//        }
+    }
+
+    private static void bfs(Coor coor) {
+        boolean visited[][] = new boolean[N][M];
+        Queue<Coor> q = new ArrayDeque<>();
+        q.add(coor);
+        visited[0][0] = true;
+
+        while(!q.isEmpty()) {
+            Coor current = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int x = current.x + dr[i];
+                int y = current.y + dc[i];
+
+                if (x < 0 || y < 0 || x >= N || y >= M) continue; // 범위넘어가는 곳
+                if (visited[x][y]) continue; // 방문했던 곳
+                if (arr[x][y] == 0) continue; // 이동할 수 없는 칸
+
+                if(arr[x][y] < arr[current.x][current.y] + 1) {
+                    arr[x][y] = arr[current.x][current.y] + 1;
+                }
+
+
+                q.offer(new Coor(x, y));
+                visited[x][y] = true;
+            }
+        }
+    }
 }
