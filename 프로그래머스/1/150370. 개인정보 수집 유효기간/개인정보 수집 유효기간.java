d552226a523366e00ec7020b41359d0c99314343
대경 <110614907+c0mmedes@users.noreply.github.com>
2023-10-25 01:59:55 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 2.89 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -15,7 +15,7 @@ class Solution {
                 // arr2[0] 약관
                 // arr2[1] 유효기간
                 if(arr2[0].equals(arr1[1])) {
-                    // 수집날짜에 유효기간을 더한 다음에 그게 today보다 작거나 같으면 그 때의 j를 list에 넣는다.
+
                     // day[0] = 년도, day[1] = 월, day[2] = 일
                     int plusDay = Integer.parseInt(arr2[1]) * 28;
                     
@@ -23,29 +23,20 @@ class Solution {
                     int timeYear = Integer.parseInt(time[0]);
                     int timeDay = Integer.parseInt(time[2]) + plusDay; 
                     int timeDayToMonth = timeDay/28; // 기존 달에 추가할 달 수
-                    int timeDay2 = timeDay%28; // 일자
+                    timeDay = timeDay % 28; // 일자
                     int timeMonth = Integer.parseInt(time[1]) + timeDayToMonth;
-                    timeYear += timeMonth/12;
-                    int timeMonth2 = timeMonth%12;
-                    System.out.println(timeYear + "먕" + timeMonth2 + "먕" + timeDay2); // 파기날짜
+                    timeYear += timeMonth / 12;
+                    timeMonth = timeMonth % 12;
 
                     String now[] = today.split("\\.");
                     int nowYear = Integer.parseInt(now[0]);
                     int nowMonth = Integer.parseInt(now[1]);
                     int nowDay = Integer.parseInt(now[2]);
-                    
-                    // if(nowYear<=timeYear && nowMonth) 
-                    
-//                     // day[0] = 년도, day[1] = 월, day[2] = 일
-//                     String day[] = arr1[0].split("\\.");
 
-                    int daySum = timeYear*12*28 + timeMonth2*28 + timeDay2;
-                    
-//                     String now[] = today.split("\\.");
+                    int daySum = timeYear*12*28 + timeMonth*28 + timeDay;
                     
                     int nowSum = Integer.parseInt(now[0])*12*28 + Integer.parseInt(now[1])*28 + Integer.parseInt(now[2]); 
                         
-//                     System.out.println(daySum + "먕" + nowSum);
                     if(daySum <= nowSum) {
                         list.add(j+1);
                     }
