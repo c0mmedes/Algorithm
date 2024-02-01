ea2eb1c991cf781b83526b2f0f38238483b11dd3
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-02-01 23:46:12 +0900

    [D3] Title: 햄버거 다이어트, Time: 738 ms, Memory: 21,132 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,38 +1,65 @@
-import java.io.FileInputStream;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-		public static void main(String[] args) throws Exception {
-			Scanner sc = new Scanner(System.in);
-			int T = sc.nextInt();
-			
-			
-			for (int tc = 1; tc <= T; tc++) {
-				int N = sc.nextInt(); // 재료의 수
-				int L = sc.nextInt(); // 제한 칼로리
-				
-				int arr[][] = new int[N][2];
-				
-				for (int i = 0; i < N; i++) {
-					arr[i][0] = sc.nextInt(); // 점수
-					arr[i][1] = sc.nextInt(); // 칼로리
-				}
-				
-				int ans = 0;
-				
-				for (int i = 0; i < (1 << N); i++) {
-					int calsum = 0;
-					int score = 0;
-					for (int j = 0; j < N; j++) {
-						if ((i & (1 << j)) != 0) {
-							calsum += arr[j][1];
-							score += arr[j][0];
-						}
-					}
-					if (calsum <= L) ans = Math.max(ans, score);
-				}
-				
-				System.out.println("#" + tc + " " + ans);
-			}
-	}
+    static boolean visited[];
+    static int arrT[];
+    static int arrK[];
+    static int N, L, answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t ++) {
+            answer = 0;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken()); // 재료의 수
+            L = Integer.parseInt(st.nextToken()); // 제한 칼로리
+
+            arrT = new int[N]; // 맛
+            arrK = new int[N]; // 칼로리
+
+            visited = new boolean[N];
+
+            // 맛과 칼로리 저장
+            for (int n = 0; n < N; n++) {
+                st = new StringTokenizer(br.readLine());
+                arrT[n] = Integer.parseInt(st.nextToken()); // 맛
+                arrK[n] = Integer.parseInt(st.nextToken()); // 칼로리
+            }
+
+            subset(0);
+
+            System.out.println("#" + t + " " + answer);
+        }
+    }
+
+    private static void subset(int cnt) {
+        if (cnt == N) {
+            int sumT = 0; // 맛의 합
+            int sumK = 0; // 칼로리의 합
+            for (int i = 0; i < N; i++) {
+                if(!visited[i]) continue;
+
+                sumK += arrK[i];
+
+                if (sumK <= L) {
+                    sumT += arrT[i];
+                }
+            }
+            if (sumT > answer) {
+                answer = sumT;
+            }
+            return;
+        }
+        visited[cnt] = true;
+        subset(cnt + 1);
+        visited[cnt] = false;
+        subset(cnt + 1);
+    }
 }
