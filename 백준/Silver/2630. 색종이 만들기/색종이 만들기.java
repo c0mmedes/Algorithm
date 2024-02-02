f52df72883ea7e3973e00b17cd995e852f11a522
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-02-02 23:04:59 +0900

    [Silver II] Title: 색종이 만들기, Time: 212 ms, Memory: 19372 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,56 +1,50 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
-	static int N;
-	static int map[][];
-	static int whiteCnt = 0;
-	static int blueCnt = 0;
-	public static void main(String[] args) throws Exception {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			
-			N = Integer.parseInt(br.readLine()); 
-			map = new int[N][N];
-			
-			for (int i = 0; i < N; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			cut(0, 0, N);
-			System.out.println(whiteCnt);
-			System.out.println(blueCnt);
-	}
-	private static void cut(int r, int c, int size) {
-		int whiteSum = 0;
-		int blueSum = 0;
-		
-		for (int i = r, rEnd = r+size; i < rEnd; i++) {
-			for (int j = c, cEnd = c+size; j < cEnd; j++) {
-				if(map[i][j] == 0) {
-					whiteSum++;
-				} else {
-					blueSum++;
-				}
-			}
-		}
-		
-		if(whiteSum == size*size) {
-			whiteCnt++;
-		} else if (blueSum == size*size) {
-			blueCnt++;
-		} else {
-			int half = size / 2;
-			cut(r, c, half);
-			cut(r + half, c, half);
-			cut(r, c + half, half);
-			cut(r + half, c + half, half);
-		}
-		
-		
-	}
+    static int blue = 0;
+    static int white = 0;
+    static int arr[][];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new int[N][N];
+
+        for (int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        func(0, 0, N);
+
+        System.out.println(white + "\n" + blue);
+    }
+
+    private static void func(int r, int c, int size) {
+        int zeroCount = 0; // 하얀색
+        int oneCount = 0; // 파란색
+
+        for (int i = r, rEnd = r + size; i < rEnd; i++){
+            for (int j = c, cEnd = c + size; j < cEnd; j++){
+                if (arr[i][j] == 0) zeroCount++;
+                if (arr[i][j] == 1) oneCount++;
+            }
+        }
+
+        if (zeroCount == (size*size)) white++;
+        else if(oneCount == (size*size)) blue++;
+        else {
+            int half = size / 2;
+            func(r, c, half);
+            func(r + half, c, half);
+            func(r, c + half, half);
+            func(r + half, c + half, half);
+        }
+    }
 }
