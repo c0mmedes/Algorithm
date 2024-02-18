7155dbc409305c5f3e920666bb0e580354a2adbc
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-02-19 02:50:59 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 116 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,44 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	static int numbers[], inputs[], answer[];
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		
-		numbers = new int[7];
-		inputs = new int[9];
-		answer = new int[7];
-		
-		for (int i = 0; i < 9; i++) inputs[i] = sc.nextInt();
-		
-		comb(0, 0);
-		
-	}
+    static int inputs[], numbers[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	private static void comb(int cnt, int start) {
+        inputs = new int[9];
+        numbers = new int[7];
 
-		if(cnt == 7) {
-			int sum = 0;
-			for(int i = 0; i < 7; i++) {
-				sum += numbers[i];
-				answer[i] = numbers[i];
-			}
-			
-			if (sum == 100) {
-				for (int i = 0; i <answer.length; i++) {
-					System.out.println(answer[i]);
-				}
-			}
-		return;
-		
-		}
-		
-		for (int i = start; i < 9; i++) {
-			numbers[cnt] = inputs[i];
-			comb(cnt + 1, i + 1);
-		}
-		
-	}
+        for (int i = 0; i < 9; i++){
+            inputs[i] = Integer.parseInt(br.readLine());
+        }
+
+        comb(0, 0);
+    }
+
+    private static void comb(int cnt, int start) {
+        if (cnt == 7) {
+            int sum = 0;
+
+            for (int num : numbers) sum += num;
+
+            if(sum == 100) {
+                for (int num : numbers) System.out.println(num);
+                return;
+            }
+            return;
+        }
+
+        for (int i = start; i < 9; i++) {
+            numbers[cnt] = inputs[i];
+            comb(cnt + 1, i + 1);
+        }
+    }
 }
