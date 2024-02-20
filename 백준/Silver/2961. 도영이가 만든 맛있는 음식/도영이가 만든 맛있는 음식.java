39baad77a7b27f892a5d97b22203f4f76a8c3af3
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-02-20 20:39:19 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,35 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt(); // 재료의 개수
-		
-		int arr[][] = new int[N][2];
-		
-		for (int i = 0; i < N; i++) {
-			arr[i][0] = sc.nextInt(); // 신맛 
-			arr[i][1] = sc.nextInt(); // 쓴맛
-		}
-		
-		int ans = Integer.MAX_VALUE;
-		
-		for (int i = 1; i < (1 << N); i++) {
-			int sin = 1;
-			int sseun = 0;
-			for (int j = 0; j < N; j++) {
-				if ((i & (1 << j)) != 0) {
-					sin *= arr[j][0];
-					sseun += arr[j][1];
-				}
-			}
-			
-			ans = Math.min(Math.abs(sin - sseun), ans);
-		}
-		
-		System.out.println(ans);
-		
-		
-	}
+    static int N, arrS[], arrB[];
+    static int answer = Integer.MAX_VALUE;
+    static boolean visited[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine()); // 재료의 수
+
+        visited = new boolean[N];
+
+        arrS = new int[N];
+        arrB = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arrS[i] = Integer.parseInt(st.nextToken()); // 신맛
+            arrB[i] = Integer.parseInt(st.nextToken()); // 쓴맛
+        }
+        subset(0);
+
+        System.out.println(answer);
+    }
+
+    private static void subset(int cnt) {
+        if (cnt == N) {
+            int sumS = 1; // 신맛의 곱
+            int sumB = 0; // 쓴맛의 합
+            int diff = 0;
+            for (int i = 0; i < N; i++) {
+                if(!visited[i]) continue;
+                sumS *= arrS[i];
+                sumB += arrB[i];
+                diff = Math.abs(sumS - sumB);
+                if (answer > diff) answer = diff;
+            }
+            return;
+        }
+
+        visited[cnt] = true;
+        subset(cnt + 1);
+        visited[cnt] = false;
+        subset(cnt + 1);
+    }
 }
