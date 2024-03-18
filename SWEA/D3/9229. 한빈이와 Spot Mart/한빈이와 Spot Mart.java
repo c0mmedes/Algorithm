d74f90f17f467b5ab463586a5efe342fa7c279f7
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-03-18 16:26:16 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 189 ms, Memory: 33,440 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -1,50 +1,51 @@
-import java.io.FileInputStream;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-	
-	static int[] inputs, numbers;
-	static int N, M, max;
-	
-	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("data/sw-input9229.txt"));
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		
-		for (int tc = 1; tc <= T; tc++) {
-			
-			N = sc.nextInt(); // 과자 봉지 개수
-			M = sc.nextInt(); // 무게 합 제한
-			
-			inputs = new int[N];
-			numbers = new int[2];
-			max = -1;
-			
-			for(int i = 0; i < N; i++) {
-				inputs[i] = sc.nextInt();
-			}
-			comb(0, 0);
-			System.out.println("#" + tc + " " + max);
-		}
-		
-		
-	}
-	
-	public static void comb(int cnt, int start) {
-		if(cnt == 2) {
-			
-			int ans = numbers[0] + numbers[1];
-			if (ans <= M && ans >= max) {
-				max = ans;
-			}
-			return;
-		}
-		
-		for (int i = start; i < N; i++) {
-			numbers[cnt] = inputs[i];
-			comb(cnt+1, i+1);
-		}
-	}
+    static int N, max, M, weight[], chocie[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t ++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken()); // 봉지의 개수
+            M = Integer.parseInt(st.nextToken()); // 무게 합 제한
+
+            weight = new int[N]; // 맛
+            chocie = new int[2]; // 맛
+
+            st = new StringTokenizer(br.readLine());
+            for (int n = 0; n < N; n++) {
+                weight[n] = Integer.parseInt(st.nextToken()); // 각 봉지의 무게
+            }
+
+            max = Integer.MIN_VALUE;
+            comb(0, 0);
+
+            if (max < 0) max = -1;
+
+            System.out.println("#" + t + " " + max);
+        }
+    }
+
+    private static void comb(int cnt, int start) {
+        if (cnt == 2) {
+            int sum = 0;
+            for (int num : chocie) sum += num;
+
+            if(sum <= M && sum >= max) max = sum;
+
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            chocie[cnt] = weight[i];
+            comb(cnt + 1, i + 1);
+        }
+
+    }
 }
