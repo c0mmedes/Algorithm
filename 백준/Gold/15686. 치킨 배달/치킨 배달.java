a3fa8501e6852371e68dd2fe846dc7bef6d56537
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-03-20 17:26:40 +0900

    [Gold V] Title: 치킨 배달, Time: 208 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,85 +1,83 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Coord {
-	int x, y;
-
-	public Coord(int x, int y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
-}
-
 public class Main {
-	static int chickenCnt, homeCnt;
-	static int N, R;
-	static Coord[] home, chicken, numbers;
-	static int ans;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		N = Integer.parseInt(st.nextToken()); // NxN 크기의 공간
-		R = Integer.parseInt(st.nextToken()); // 뽑아야 할 치킨집의 수
-		
-		chickenCnt = 0;
-		homeCnt = 0;
-		chicken = new Coord[13]; // 치킨집 좌표저장
-		home = new Coord[2*N]; // 집 좌표저장
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < N; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				if (num == 2) chicken[chickenCnt++] = new Coord(i, j);
-				if (num == 1) home[homeCnt++] = new Coord(i, j);
-			}
-		}
-		
-		numbers = new Coord[R];
-		ans = Integer.MAX_VALUE;
-		comb(0, 0);
-		System.out.println(ans);
-		
-	}
-	private static void comb(int cnt, int start) {
-		if (cnt == R) {
-			int sum = 0;
-			
-//			for (int i = 0; i < R; i++) {
-//				int chickenX = numbers[i].x;
-//				int chickenY = numbers[i].y;
-//				int min = Integer.MAX_VALUE;;
-//				for (int j = 0; j < homeCnt; j++) {
-//					int homeX = home[j].x;
-//					int homeY = home[j].y;
-//					min = Math.min(Math.abs(homeX - chickenX) + Math.abs(homeY - chickenY), min);
-//				}
-//				sum += min;
-//			}
-			
-			for (int i = 0; i < homeCnt; i++) {
-                int homeX = home[i].x;
-                int homeY = home[i].y;
-                int min = Integer.MAX_VALUE;
-                for (int j = 0; j < R; j++) {
-                    int chickenX = numbers[j].x;
-                    int chickenY = numbers[j].y;
-                    min = Math.min(Math.abs(homeX - chickenX) + Math.abs(homeY - chickenY), min);
+    static class chicken {
+        int r;
+        int c;
+
+        public chicken(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+    static int N, M, chickenCnt, ans;
+    static chicken chicken[];
+    static chicken chickenM[];
+    static int map[][];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken()); // 치킨집의 최소 개수
+
+        chickenCnt = 0;
+
+        map = new int[N+1][N+1];
+        chicken = new chicken[13];
+        chickenM = new chicken[M];
+
+        // 도시의 정보, 0 - 빈칸, 1 - 집, 2 - 치킨집
+        // 1 <= 집 <= 2N, M <= 치킨집 <= 13
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                map[i][j] = num;
+                if (num == 2) {
+                    chicken[chickenCnt] = new chicken(i, j);
+                    chickenCnt++;
                 }
-                sum += min;
             }
-			
-			ans = Math.min(sum, ans);
-			return;
-		}
-		
-		
-		for (int i = start; i < chickenCnt; i++) {
-			numbers[cnt] = chicken[i];
-			comb(cnt + 1, i + 1);
-		}
-	}
+        }
+
+        ans = Integer.MAX_VALUE;
+
+        comb(0, 0);
+
+        System.out.println(ans);
+    }
+
+    private static void comb(int cnt, int start) {
+        if(cnt == M) {
+            int sum = 0;
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if(map[i][j] == 1) {
+                    int min = Integer.MAX_VALUE;
+                        for (chicken c : chickenM) {
+                            int dis = Math.abs(i - c.r) + Math.abs(j - c.c);
+                            min = Math.min(min, dis);
+                        }
+                        sum += min;
+                    }
+                }
+            }
+
+            ans = Math.min(ans, sum);
+
+            return;
+        }
+
+        for (int i = start; i < chickenCnt; i++) {
+            chickenM[cnt] = chicken[i];
+            comb(cnt + 1, i + 1);
+        }
+    }
 }
+
+// 치킨 거리 - 집과 가장 가까운 치킨집 사이의 거리
+// 도시의 치킨 거리는 모든 치킨 거리의 합
