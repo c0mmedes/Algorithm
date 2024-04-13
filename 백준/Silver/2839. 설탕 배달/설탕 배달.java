ba6bd8c0250e4720fdf721f13884813c601f6428
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-04-13 15:13:39 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,26 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt(); // 정확히 Nkg 배달
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int ans = 0;
-		
-		while(true) {
-			if (N % 5 == 0) {
-				ans += N/5;
-				System.out.println(ans);
-				break;
-			} else {
-				N -= 3;
-				ans++;
-			}
-			if (N < 0) {
-				System.out.println("-1");
-				break;
-			}
-		}
-	}
+        int N = Integer.parseInt(br.readLine());
+        int ans = 0;
+
+        while(true) {
+
+            if(N%5==0) {
+                ans += N/5;
+                break;
+            } else {
+                N -= 3;
+                ans++;
+            }
+
+            if (N < 0) {
+                ans = -1;
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+
 }
+
+// 3kg, 5kg 봉지
+// 최대한 적은 봉지
+// ex) N == 18 -> 5kg * 3 + 3kg * 1 최소
