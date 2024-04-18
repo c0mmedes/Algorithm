274e1a3b86b38595137b0797a7588ee6232d428b
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-04-18 13:36:51 +0900

    [Silver IV] Title: 수 찾기, Time: 748 ms, Memory: 42800 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,36 +1,57 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int arr[] = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		Arrays.sort(arr);
-		
-		int M = Integer.parseInt(br.readLine());
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		
-		for (int i = 0; i < M; i++) {
-			if (Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) < 0) {
-				sb.append(0).append("\n");
-			} else {
-				sb.append(1).append("\n");
-			}
-		}
-		
-		System.out.println(sb);
-	}
+        int N = Integer.parseInt(br.readLine());
+
+        int A[] = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            A[i] = num;
+        }
+
+        Arrays.sort(A);
+
+        int M = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            // 배열 A에 num이 존재하는지 알아내기
+            int num = Integer.parseInt(st.nextToken());
+
+            int start = 0;
+            int half = 0;
+            int end  = N-1;
+            while(true) {
+                if (start > end){
+                    sb.append(0 + "\n");
+                    break;
+                }
+
+                half = (start+end)/2;
+
+                if (num == A[half]) {
+                    sb.append(1 + "\n");
+                    break;
+                } else if (num < A[half]) {
+                    end = half - 1;
+                } else {
+                    start = half + 1;
+                }
+
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
