ad929f5a9bec6a469dfef2a1ad71b396438b00dc
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-04-19 14:24:44 +0900

    [Silver I] Title: Z, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,36 +1,38 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, r, c, cnt;
-	static int map[][];
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-	        
-		N = Integer.parseInt(st.nextToken());  // 2^n * 2^n 의 2차원 배열
-		r = Integer.parseInt(st.nextToken());  // r행
-		c = Integer.parseInt(st.nextToken());  // c열
-		cnt = 0;
-		int size = (int) Math.pow(2, N);
-		check(0, 0, size);
-	}
-	
-	private static void check(int row, int col, int size) {
-		if (row == r && col == c) {
-            System.out.println(cnt);
+    static int N, r, c, ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        ans = 0;
+
+        int size = (int) Math.pow(2,N);
+
+        dfs(0, 0, size);
+    }
+
+    private static void dfs(int R, int C, int size) {
+        if(R == r && C == c) {
+            System.out.println(ans);
             return;
-		}
-		
- 	    if (row <= r && r < (row + size) && col <= c && c < (col + size)) {
- 	    	int half = size / 2;
- 	    	check(row, col, half);
- 	    	check(row, col + half, half);
- 	    	check(row + half, col, half);
- 	    	check(row + half, col + half, half);
- 	    } else {
- 	    	cnt += size*size;
- 	    }
-	}
+        }
+
+        if (R <= r && r < (R + size) && C <= c && c < (C + size)) {
+            int half = size/2;
+            dfs(R, C, half);
+            dfs(R, C + half, half);
+            dfs(R + half, C, half);
+            dfs(R + half, C + half, half);
+        } else {
+            ans += size*size;
+        }
+    }
 }
