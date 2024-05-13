ea040c45ee33fb2f00d67fe2eed8b876f2fdccce
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-05-14 00:41:09 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT FLAVOR
+FROM FIRST_HALF 
+WHERE TOTAL_ORDER > 3000 AND FLAVOR IN ('peach', 'watermelon', 'mango', 'strawberry', 'melon', 'orange', 'pineapple')
+ORDER BY TOTAL_ORDER DESC
