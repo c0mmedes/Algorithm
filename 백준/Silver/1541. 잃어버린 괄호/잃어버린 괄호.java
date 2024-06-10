744f6cb7915b483a391262f6e6c361acef106ef0
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-06-10 23:35:08 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 80 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,48 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		
-		int fir = 0;
-		String num = "";
-		int sum = 0;
-		
-		for (int i = 0; i < s.length(); i++) {
-			if(s.charAt(i) == '-') {
-				sum += Integer.parseInt(num);
-				fir = i;
-				num = "";
-				break;
-			}
-			else if(s.charAt(i) == '+') {
-				sum += Integer.parseInt(num);
-				num = "";
-			} else {
-				num += s.charAt(i);
-			}
-		}
-		
-		if (!num.equals("")) {
-			sum += Integer.parseInt(num);
-			System.out.println(sum);
-			return;
-		}
-		
-		if (fir != 0) {
-			for (int i = fir + 1; i < s.length(); i++) {
-				if(s.charAt(i) == '+' || s.charAt(i) == '-') {
-					sum -= Integer.parseInt(num);
-					num = "";
-				} else {
-					num += s.charAt(i);
-				}
-			}
-		}
-		
-		if (!num.equals("")) sum -= Integer.parseInt(num);
-		
-		System.out.println(sum);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int num = 0;
+        String res = "";
+        boolean flag2 = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '-' || ch == '+') {
+                if (flag2) {
+                    num -= Integer.parseInt(res);
+                } else {
+                    num += Integer.parseInt(res);
+                }
+                res = "";
+                if (ch == '-') {
+                    flag2 = true;
+                }
+            } else {
+                res += ch;
+            }
+        }
+
+        // 남은 값 처리
+        if (!res.isEmpty()) {
+            if (flag2) {
+                num -= Integer.parseInt(res);
+            } else {
+                num += Integer.parseInt(res);
+            }
+        }
+
+        System.out.println(num);
+    }
 }
