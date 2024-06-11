03591d5a4f351ccf9850ae7fcb654c0223aa3e1c
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-06-11 23:47:26 +0900

    [Gold III] Title: 아기 상어, Time: 808 ms, Memory: 296104 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,158 +1,134 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-class Coor {
-	int r;
-	int c;
-	
-	public Coor(int r, int c) {
-		super();
-		this.r = r;
-		this.c = c;
-	}
-}
+import java.util.*;
 
 public class Main {
-	static int dr[] = {1, 0, -1, 0};
-	static int dc[] = {0, 1, 0, -1}; 
-	static int N, ans, killCount,sx, sy = 0;
-	static int minR, micC, minDist;
-	static int sharkSize = 2;
-	static int [][] map, check;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine()); // NxN 크기의 공간
-		
-		map = new int[N][N];
-		
-		
-		Coor babyShark = null;
-		
-		
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 9) {
-					babyShark = new Coor(i, j);
-//					sx = i;
-//					sy = j;
-					map[i][j] = 0;
-				}
-			}
-		}
-		
-		while(true) {
-			init();
-			
-			eat(babyShark);
-//			eat(sx, sy);
-			
-			if(minR != Integer.MAX_VALUE && micC != Integer.MAX_VALUE) {
-				killCount++;
-				
-				map[minR][micC] = 0;
-				
-				// 현재의 좌표 넘겨주기
-//				sx = minR;
-//				sy = micC;
-				babyShark.r = minR;
-				babyShark.c = micC;
-				
-				ans += check[minR][micC];
-				
-				if (killCount == sharkSize) {
-					sharkSize++;
-					killCount = 0;
-				}
-				
-			}
-			
-			else {
-				break;
-			}
-			
-		}
-		
-		System.out.println(ans);
-		
-	}
+    static int N, ans;
+    static int dr[] = {0, 0, -1, 1};
+    static int dc[] = {-1, 1, 0, 0};
+    static int map[][];
+    static Coor baby;
+    static List<Coor> sharks = new ArrayList<>();
+    static class Coor {
+        int r, c, size, exp, d;
 
-	private static void init() {
-		minDist = Integer.MAX_VALUE;
-		minR = Integer.MAX_VALUE;
-		micC = Integer.MAX_VALUE;
-		
-		check = new int[N][N];
-	}
+        public Coor (int r, int c, int size, int exp, int d) {
+            this.r = r;
+            this.c = c;
+            this.size = size;
+            this.exp = exp; // 먹은 마리수
+            this.d = d; // 상어와의 거리
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	private static void eat(Coor baby) {
-//		check[baby.c][baby.r] = 0;
-//		check[x][y] = 0;
-		
-		Queue<Coor> q = new ArrayDeque<>();
-		q.offer(baby);
-//		q.offer(new Coor(r, c));
-		
-		while (!q.isEmpty()) {
-			Coor cur = q.poll();
-			
-			for (int d = 0; d < 4; d++) {
-				int nr = cur.r + dr[d];
-				int nc = cur.c + dc[d];
-				
-				// 다음 칸이 범위 안에 있고 방문하지 않았고 그 칸의 값이 현재 아기상어 사이즈보다 작거나 같을 때 
-				if(0 <= nr && nr < N && 0 <= nc && nc < N && check[nr][nc] == 0 && map[nr][nc] <= sharkSize) {
-					// 그 다음 칸을 현재 칸의 값 보다 +1 해줌
-					check[nr][nc] = check[cur.r][cur.c] + 1; 
-					
-					// 아기 상어가 먹을 수 있을 때
-					if (0 < map[nr][nc] && map[nr][nc] < sharkSize) {
-						// 거리가 더짧을 때
-						if(minDist > check[nr][nc]) {
-							minDist = check[nr][nc];
-							minR = nr;
-							micC = nc;
-							// 거리가 같을 때
-						} else if (minDist == check[nr][nc]) {
-							if(minR == nr) {
-								if (micC > nc) {
-									minR = nr;
-									micC = nc;
-								} // 거리도 같고 높이도 같을 때
-							} else if (minR > nr) {
-								minR = nr;
-								micC = nc;
-							}
-						}
-						
-					}
-					
-					q.offer(new Coor(nr, nc));
-				}
-				
-				
-			}
-		}
-		
-	}
+        N = Integer.parseInt(st.nextToken()); // N * N 공간
+        map = new int[N][N];
 
+        ans = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 9) baby = new Coor(i, j,2, 0, 0); // 아기상어의 위치
+                else if(map[i][j] != 0) sharks.add(new Coor(i, j, map[i][j],0, 0)); // 물고기들의 위치
+            }
+        }
+
+        map[baby.r][baby.c] = 0;
+
+        while(true) {
+            PriorityQueue<Coor> q = new PriorityQueue<>(new Comparator<Coor>() {
+                //  1.거리 2.행이 작을수록 3. 열이 작을수록
+                @Override
+                public int compare(Coor o1, Coor o2) {
+                    if (o1.d != o2.d) return Integer.compare(o1.d, o2.d);
+                    else if (o1.r != o2.r) return Integer.compare(o1.r, o2.r);
+                    else return Integer.compare(o1.c, o2.c);
+                }
+            });
+
+            // 현재 베이비의 사이즈보다 작고 이동가능한 곳만 큐에 넣기
+            for (int i = sharks.size()-1; i >= 0; i--) {
+                Coor shark = sharks.get(i);
+                if((shark.size < baby.size)) {
+                    int distance = bfs(shark);
+                    if(distance != 0) {
+                        shark.d = distance;
+                        q.add(shark);
+                        sharks.remove(i);
+                    }
+                }
+            }
+
+            // 종료조건
+            if(q.isEmpty()) break;
+
+            Coor shark = q.poll();
+            baby.r = shark.r;
+            baby.c = shark.c;
+            baby.exp++;
+            if(baby.exp == baby.size) {
+                baby.exp = 0;
+                baby.size++;
+            }
+            ans += shark.d;
+
+            // 우선순위에서 밀린 애들 다시 넣어주기
+            while(!q.isEmpty()) {
+                sharks.add(q.poll());
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    private static int bfs(Coor shark) {
+        int distance = 1;
+        boolean visited[][] = new boolean[N][N];
+        Queue<Coor> q = new ArrayDeque<>();
+        q.add(baby);
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                Coor shk = q.poll();
+                for (int d = 0; d < 4; d++){
+                    int nr = shk.r + dr[d];
+                    int nc = shk.c + dc[d];
+
+                    if(nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+                    if(map[nr][nc] > baby.size) continue;
+                    if(visited[nr][nc]) continue;
+                    if(nr == shark.r && nc == shark.c) return distance;
+
+                    q.offer(new Coor(nr, nc, 0, 0, 0));
+                    visited[nr][nc] = true;
+                }
+            }
+            distance++;
+        }
+        return 0;
+    }
 }
 
+// 조건
+// 아기상어의 처음 크기 2
+// 아기 상어의 위치 9
+// 상하좌우로 1초에 1칸 이동가능
+// 자신과 같거나 작은 칸만 지나갈 수 있다.
+// 자신보다 작은 칸만 먹을 수 있다.
+// 자신의 크기가 2일 떄 2마리를 먹어야 크기가 1올라감
+// 더 이상 먹을 수 있는 물고기가 공간에 없다면 엄마 상어에게 도움 요청 (종료 조건)
+// 잡아먹은 시간 초 출력
 
-// 아기 상어 크기는 2
-// 자기보다 작거나 같은 물고기가 있는 곳만 지나갈수있음(먹는 건 작은애들만 먹을수있음)
-// 먹을 수 있는 애가 없으면 엄마상어에게 도옴요청 
-// 먹을 수 있는 물고기가 1마리면 그 물고기 먹으러감
-// 먹을 수 있는 물고기가 1마리보다 많으면 가장 가까운(지나야하는 칸의 개수의 최소값) 애 먹으러감
-// 거리 가까운 애들이 중복이면 가장 위에있는 물고기가 우선 그 다음으로는 가장 왼쪽
-// 먹고나면 거긴 빈칸됨
-// 아기 상어는 자신의 크기만큼 먹으면 크기가 +1됨.
-// 아기 상어의 위치 = 9
-// 물고기는 최대 6크기
-// -> 아기 상어가 몇 초 동안 엄마 상어에게 도움을 요청하지 않고 물고기를 잡아먹을 수 있는지 구하세요.
+// 이동 경로
+// 먹을 수 있는 물고기가 1마리일 경우 그 물고기를 먹으러 가고
+// 초과일 경우 가장 가까운 물고기 먹방(|x1 - x2| + |y1 - y2|)
+// 거리는 아기 상어가 있는 칸에서 물고기가 있는 칸으로 이동할 때 지나야 하는 칸의 개수
+// 거리가 가까운 물고기가 많다면 1. 상 2. 좌 우선으로 먹는다.
+// 먹으면 그 칸은 빈칸이 됨
