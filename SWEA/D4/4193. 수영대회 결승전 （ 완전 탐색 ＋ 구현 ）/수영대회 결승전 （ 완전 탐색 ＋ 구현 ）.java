ab63d3fc4fda8ead3378880bc5a91471366982fe
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-07-16 21:15:34 +0900

    [D4] Title: 수영대회 결승전 ( 완전 탐색 + 구현 ), Time: 111 ms, Memory: 19,744 KB -BaekjoonHub

--- SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
+++ SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
@@ -1,8 +1,5 @@
-// 지나갈 수 있는 곳 0
-// 섬과 같은 장애물은 1
-// 소용돌이 같은 장애물은 2 (2초동안 유지되다가 1초동안 잠잠)
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
@@ -10,98 +7,96 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int[][] map;
-	static boolean[][] visited;
-	static int N, A, B, C, D;
-	static int dr[] = {0, 0, -1, 1};
-	static int dc[] = {1, -1, 0, 0};
-	
-	static class Coor {
-		int x, y;
+    static int arr[][];
+    static int N, answer, A, B, C, D;
+    static int dx[] = {-1, 1, 0, 0};
+    static int dy[] = {0, 0, -1, 1};
+    static class Coor {
+        int x, y;
 
-		public Coor(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
-	}
+        public Coor(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("data/4193.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine()); // TEST CASE
-		
-		for (int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine()); // N*N 수영장
-			map = new int[N][N];
-			visited = new boolean[N][N];
-			
-			StringTokenizer st = null;
-			
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine(), " ");
-				for (int j = 0; j < N; j++) {
-					int num = Integer.parseInt(st.nextToken());
-					map[i][j] = num;
-				}
-			}
-			
-			// 시작위치
-			st = new StringTokenizer(br.readLine(), " ");
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
-			
-			// 도착위치
-			st = new StringTokenizer(br.readLine(), " ");
-			C = Integer.parseInt(st.nextToken());
-			D = Integer.parseInt(st.nextToken());
-			
-			System.out.println("#" + tc + " " + bfs(new Coor(A, B)));
-		}
-	}
+        int T = Integer.parseInt(br.readLine());
 
-	private static int bfs(Coor coor) {
-		Queue<Coor> q = new ArrayDeque<>();
-		q.offer(coor);
-		visited[coor.x][coor.y] = true; 
-		int time = 0;
-		while (!q.isEmpty()) {
-			int size = q.size();
-			
-			while(size-- > 0) {
-				Coor current = q.poll();
-				int X = current.x;
-				int Y = current.y;
-				
-				for (int d = 0; d < 4; d++) {
-					int nx = X + dr[d];
-					int ny = Y + dc[d];
-					
-					// 경계 체크
-					if (0 > nx || nx >= N || 0 > ny || ny >= N) continue;
-					
-					// 갈 수 있는 곳인지 체크 
-					if (map[nx][ny] == 1 || visited[nx][ny]) continue;
-					
-					// 소용돌이 인데 쉬지 않을 때
-					if (map[nx][ny] == 2 && time%3!=2) {
-						q.offer(current);
-						continue;
-					}
-					
-					// 조건이 맞춰졌을 때
-					if (nx == C && ny == D) {
-						return time+1;
-					}
-					
-					// 일반적인 상황
-					q.offer(new Coor(nx, ny));
-					visited[nx][ny] = true; 
-				}
-			}
-			time++;
-		}
-		
-		return -1;
-	}
+        for (int t = 1; t <= T; t ++) {
+            answer = 0;
+
+            N = Integer.parseInt(br.readLine()); // 수영장의 크기
+
+            arr = new int[N][N]; // 맛
+
+            // 0: 지나갈 수 있는 곳
+            // 1: 장애물
+            // 2: 주기가 2초인 소용돌이
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            st = new StringTokenizer(br.readLine());
+            // 시작 위치
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            // 도착 위치
+            C = Integer.parseInt(st.nextToken());
+            D = Integer.parseInt(st.nextToken());
+
+            answer = bfs();
+
+            System.out.println("#" + t + " " + answer);
+        }
+    }
+
+    private static int bfs() {
+        boolean visited[][] = new boolean[N][N];
+        Queue<Coor> q = new ArrayDeque<>();
+        q.offer(new Coor(A, B));
+        int count = 0;
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                Coor c = q.poll();
+
+                if(c.x == C && c.y == D) {
+                    return count;
+                }
+
+                if (arr[c.x][c.y] == 2) {
+                    if (count%3!=0) {
+                        q.offer(new Coor(c.x, c.y));
+                        visited[c.x][c.y] = true;
+                        continue;
+                    }
+                }
+
+                for (int d = 0; d < 4; d++) {
+                    int nx = c.x + dx[d];
+                    int ny = c.y + dy[d];
+
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                    if (visited[nx][ny] || arr[nx][ny] == 1) continue;
+
+                    q.offer(new Coor(nx, ny));
+                    visited[nx][ny] = true;
+                }
+            }
+            count++;
+        }
+        return -1;
+    }
 }
+// 소용돌이는 생성되고 2초동안 유지되다가 1초동안 잠잠(0, 1 후 2초에 사라짐)
+// 한번 통과한 소용돌이 위에서는 머물러 있을 수 있다.
+// 몇 초 만에 골인할수 있을지 찾기.
