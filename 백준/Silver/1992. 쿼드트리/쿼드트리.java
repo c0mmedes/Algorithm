827ca6dca50e3b6de5d99bfccd36169d2478fea0
대경 <110614907+c0mmedes@users.noreply.github.com>
2024-07-25 18:01:21 +0900

    [Silver I] Title: 쿼드트리, Time: 128 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,53 +1,62 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int white = 0;
-	static int green = 0;
-	static int[][] spaces;
-	static StringBuilder sb = new StringBuilder();
-	
-	static void cut(int r, int c, int size) { // 좌상단, 사이즈
-		
-		int sum = 0;
-		for (int i = r, rEnd = r + size; i < rEnd; i++) {
-			for (int j = c, cEnd = c + size; j < cEnd; j++) {
-				sum += spaces[i][j];				
-			}
-		}
-		
-		if (sum == size * size) { // 모두 1
-			sb.append(1);
-		} else if (sum == 0) { // 모두 0
-			sb.append(0);
-		} else { // 혼합된 상황
-			// 4분할
-			int half = size / 2;
-			sb.append("(");
-			cut(r, c, half);
-			cut(r, c + half, half);
-			cut(r + half, c, half);
-			cut(r + half, c + half, half);
-			sb.append(")");
-		}
-	}
+    static int N, arr[][];
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		spaces = new int[N][N];
-		
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < N; j++) {
-				spaces[i][j] = (int) str.charAt(j) - 48;
-			}
-		}
-		
-		cut(0, 0, N);
-		
-		System.out.println(sb);
-	}
+        arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        cut(0, 0, N);
+
+        System.out.println(sb);
+    }
+
+    private static void cut(int R, int C, int size) {
+        int sum = 0;
+
+        for (int i = R, rEnd = R + size; i < rEnd; i++) {
+            for (int j = C, cEnd = C + size; j < cEnd; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        if(sum == size * size) sb.append("1");
+        else if(sum == 0) sb.append("0");
+        else {
+            int half = size/2;
+
+            sb.append("(");
+            // 첫번째 구간부터 4번째 구간까지의 영역
+            cut(R, C, half);
+            cut(R, C + half, half);
+            cut(R + half,C, half);
+            cut(R + half, C + half, half);
+            sb.append(")");
+        }
+    }
 }
+/*
+        1111 0000
+        1111 0000
+        0001 1100
+        0001 1100
+                        (110(0101) 3 1 3)
+        1111 0000
+        1111 0000
+        1111 0011
+        1111 0011
+
+ */
