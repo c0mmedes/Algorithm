9df1e7c06dffe532aad3c6fe6bb047f6bdc45515
대경 <110614907+c0mmedes@users.noreply.github.com>
2025-03-03 20:15:34 +0900

    [level 2] Title: 비밀 코드 해독, Time: 67.54 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -2,22 +2,21 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    static int numbers[], answer;
+    static int numbers[], answer, len;
     public int solution(int n, int[][] q, int[] ans) {
         answer = 0;
-        int len = q[0].length;
-        numbers = new int[len];
-        comb(len, n, 0, 1, q, ans);
+        numbers = new int[q[0].length];
+        comb(n, 0, 1, q, ans);
         
         return answer;
     }
-    private static void comb(int len, int n, int cnt, int start, int[][] q, int[] ans) {
-        if (cnt == len) {
-            // 여기서 len만큼 뽑고
+    private static void comb(int n, int cnt, int start, int[][] q, int[] ans) {
+        if (cnt == numbers.length) {
             List<Integer> list = new ArrayList<>();
-            // System.out.println(Arrays.toString(numbers));
             for (int i = 0; i < numbers.length; i++) list.add(numbers[i]);
+            
             boolean check = true;
+            
             out:for (int i = 0; i < q.length; i++) {
                 int count = 0;
                 for (int j = 0; j < q[i].length; j++) {
@@ -30,9 +29,9 @@ class Solution {
                     break out;
                 }
             }
+            
             if(check) {
-                // System.out.println(Arrays.toString(numbers));
-answer++;
+                answer++;
             }
             
             return;
@@ -40,9 +39,7 @@ answer++;
         
         for (int i = start; i <= n; i++) {
             numbers[cnt] = i;
-            comb(len, n, cnt + 1, i + 1, q, ans);
+            comb(n, cnt + 1, i + 1, q, ans);
         }
     }
 }
-
-// 1. 모든 정수를 
